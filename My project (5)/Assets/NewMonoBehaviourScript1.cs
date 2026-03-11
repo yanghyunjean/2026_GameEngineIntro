8f4f47868d5f1f78fbadbc12fa05a5665e68a0f3
yanghyunjean <hyunjean.yang@gmail.com>
2026-03-11 16:09:23 +0900

    움직지기

---
+++ My project (5)/Assets/NewMonoBehaviourScript1.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript1 : MonoBehaviour
+{
+    public string playerName;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("안녕 + playername");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

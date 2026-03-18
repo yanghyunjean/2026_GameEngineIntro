db884ea06f987c33a2f29485c67ecf15515bdb09
tlsrk2007 <tlsrk2007@gmail.com>
2026-03-18 14:43:46 +0900

    2026.3.18

--- My project (5)/Assets/playercontrol.cs
+++ My project (5)/Assets/playercontrol.cs
@@ -5,11 +5,27 @@ public class playercontrol : MonoBehaviour
 {
     private Vector2 moveinput;
     public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     public void OnMove(InputValue value)
     {
         moveinput = value.Get<Vector2>();
     }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+ 
 
     // Update is called once per frame
     void Update()
